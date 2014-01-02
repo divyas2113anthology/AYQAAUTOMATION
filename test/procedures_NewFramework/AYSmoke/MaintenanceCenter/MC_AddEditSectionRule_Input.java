cf7d408363f5a8ae7b858d2ee8a78f92f778a9ab
sathish24 <sathish_durai@tvsinfotech.com>
2014-01-02 09:00:39 -0500

    Updated Section Rule Input

--- test/procedures_NewFramework/AYSmoke/MaintenanceCenter/MC_AddEditSectionRule_Input.java
+++ test/procedures_NewFramework/AYSmoke/MaintenanceCenter/MC_AddEditSectionRule_Input.java
@@ -53,7 +53,7 @@ public class MC_AddEditSectionRule_Input extends CommonSeleniumActions implement
 //			        option.click();   
 //			}
 //			selectByIndexWebdriver(attributeName_xpath, ESR_Field, Integer.parseInt(field));
-			select(ESR_Section, "regexp:"+field);
+			select(ESR_Field, "regexp:"+field);
 //			selectByVisibleTextWithSpaceWebdriver(attributeName_xpath, ESR_Field, field);
 			Thread.sleep(3000);
 //			selectByVisibleTextWebdriver(attributeName_xpath, "//select[contains(@name,'txtField')][option]", field);

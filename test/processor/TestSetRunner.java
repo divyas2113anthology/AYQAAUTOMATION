7e2f6285c89e7682a8d928304a3dcbd4a3bf2c35
rturner <rturner@hobsons.com>
2014-10-06 13:39:01 -0400

    Updated TestSetRunner

--- test/processor/TestSetRunner.java
+++ test/processor/TestSetRunner.java
@@ -1,29 +1,8 @@
 package processor;
 
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Map;
-
-import javax.xml.bind.JAXBElement;
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeConstants;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-import javax.xml.namespace.QName;
-import javax.xml.ws.BindingProvider;
 
 import com.inflectra.spirateam.mylyn.core.internal.services.soap.ArrayOfRemoteTestCase;
-import com.inflectra.spirateam.mylyn.core.internal.services.soap.ArrayOfRemoteTestCaseParameter;
-import com.inflectra.spirateam.mylyn.core.internal.services.soap.ArrayOfRemoteTestStep;
-import com.inflectra.spirateam.mylyn.core.internal.services.soap.ArrayOfRemoteTestStepParameter;
 import com.inflectra.spirateam.mylyn.core.internal.services.soap.IImportExport;
 import com.inflectra.spirateam.mylyn.core.internal.services.soap.IImportExportConnectionAuthenticateServiceFaultMessageFaultFaultMessage;
 import com.inflectra.spirateam.mylyn.core.internal.services.soap.IImportExportConnectionConnectToProjectServiceFaultMessageFaultFaultMessage;
@@ -39,13 +18,11 @@ import com.inflectra.spirateam.mylyn.core.internal.services.soap.IImportExportTe
 import com.inflectra.spirateam.mylyn.core.internal.services.soap.IImportExportTestCaseRetrieveStepParametersServiceFaultMessageFaultFaultMessage;
 import com.inflectra.spirateam.mylyn.core.internal.services.soap.IImportExportTestRunRecordAutomated1ServiceFaultMessageFaultFaultMessage;
 import com.inflectra.spirateam.mylyn.core.internal.services.soap.IImportExportTestRunRecordAutomated1ValidationFaultMessageFaultFaultMessage;
-import com.inflectra.spirateam.mylyn.core.internal.services.soap.ImportExport;
-import com.inflectra.spirateam.mylyn.core.internal.services.soap.RemoteAutomatedTestRun;
-import com.inflectra.spirateam.mylyn.core.internal.services.soap.RemoteDocument;
+import com.inflectra.spirateam.mylyn.core.internal.services.soap.IImportExportTestSetRetrieveByIdServiceFaultMessageFaultFaultMessage;
 import com.inflectra.spirateam.mylyn.core.internal.services.soap.RemoteTestCase;
 
 public class TestSetRunner {
-	public static void main (String[] args) throws IImportExportConnectionAuthenticateServiceFaultMessageFaultFaultMessage, IImportExportConnectionConnectToProjectServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveByIdServiceFaultMessageFaultFaultMessage, IImportExportSystemGetProductNameServiceFaultMessageFaultFaultMessage, IImportExportProjectRetrieveServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveStepParametersServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveParametersServiceFaultMessageFaultFaultMessage, IOException, InterruptedException, IImportExportTestCaseAddUpdateAutomationScriptServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveServiceFaultMessageFaultFaultMessage, IImportExportTestRunRecordAutomated1ServiceFaultMessageFaultFaultMessage, IImportExportTestRunRecordAutomated1ValidationFaultMessageFaultFaultMessage, IImportExportDocumentAddFileServiceFaultMessageFaultFaultMessage, IImportExportConnectionDisconnectServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveByTestSetIdServiceFaultMessageFaultFaultMessage{
+	public static void main (String[] args) throws IImportExportConnectionAuthenticateServiceFaultMessageFaultFaultMessage, IImportExportConnectionConnectToProjectServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveByIdServiceFaultMessageFaultFaultMessage, IImportExportSystemGetProductNameServiceFaultMessageFaultFaultMessage, IImportExportProjectRetrieveServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveStepParametersServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveParametersServiceFaultMessageFaultFaultMessage, IOException, InterruptedException, IImportExportTestCaseAddUpdateAutomationScriptServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveServiceFaultMessageFaultFaultMessage, IImportExportTestRunRecordAutomated1ServiceFaultMessageFaultFaultMessage, IImportExportTestRunRecordAutomated1ValidationFaultMessageFaultFaultMessage, IImportExportDocumentAddFileServiceFaultMessageFaultFaultMessage, IImportExportConnectionDisconnectServiceFaultMessageFaultFaultMessage, IImportExportTestCaseRetrieveByTestSetIdServiceFaultMessageFaultFaultMessage, IImportExportTestSetRetrieveByIdServiceFaultMessageFaultFaultMessage{
 		Integer projectIdInput = null;
 		Integer testSetIdInput = null;
 		if (args.length > 0) {  //get Spira Project Id, Test Set Id, and Test Id from command line params
@@ -63,9 +40,13 @@ public class TestSetRunner {
 		ArrayOfRemoteTestCase tests = soap.testCaseRetrieveByTestSetId(testSetIdInput);
 		Integer numTests = tests.getRemoteTestCase().size(); 
 		System.out.println("Number of tests in test set: "+numTests);
+		Integer runStatus = 0;
+		String[] resultArray;
+		resultArray = new String[numTests];
 		for (int i = 0; i < numTests; i++) {
 			RemoteTestCase testToRun = tests.getRemoteTestCase().get(i);
 			Integer testId = testToRun.getTestCaseId().getValue();
+			String testName = testToRun.getName().getValue();
 			System.out.println("TestID: "+testId);
 			String[] runParams;
 			runParams = new String[3];
@@ -73,6 +54,37 @@ public class TestSetRunner {
 			runParams[1] = testSetIdInput.toString();
 			runParams[2] = testId.toString();
 			processor.DriverScript.main(runParams);
+			//System.out.println("*************test "+testId+"status: "+testToRun.getExecutionStatusId().getValue());
 		}
+		Thread.sleep(5000);
+		for (int i = 0; i < numTests; i++) {
+			RemoteTestCase testToRun = tests.getRemoteTestCase().get(i);
+			testToRun = tests.getRemoteTestCase().get(i);
+			Integer testId = testToRun.getTestCaseId().getValue();
+			String testName = testToRun.getName().getValue();
+			Integer testStatus = testToRun.getExecutionStatusId().getValue();
+			String strTestStatus = "Failed";
+			if(testStatus.equals(2)){
+				strTestStatus = "Passed";
+			}
+			resultArray[i] = "Test: "+testName+" "+testId+" status: "+strTestStatus;
+			System.out.println(resultArray[i]);
+			if(!(testToRun.getExecutionStatusId().getValue().equals(2))){
+				runStatus = 1;
+			}
+
+		}
+
+	if(runStatus.equals(1)){
+		System.out.println("*******************");
+		System.out.println("Failure in test set");
+		System.out.println("*******************");
+		System.exit(1);
+	}else{
+		System.out.println("*******************");
+		System.out.println("Test set passed");
+		System.out.println("*******************");
+		System.exit(0);
+	}
 	}
 }

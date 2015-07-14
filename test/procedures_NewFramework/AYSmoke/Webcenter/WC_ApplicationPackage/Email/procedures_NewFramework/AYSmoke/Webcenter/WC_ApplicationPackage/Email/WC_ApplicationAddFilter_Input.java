380754216ca38f2eac4dc955a1bc7aea5be13586
Ryan Turner <rturner@hobsons.com>
2015-07-14 15:34:39 -0400

    Updated selenium server jar and misplaced packages.

---
+++ test/procedures_NewFramework/AYSmoke/Webcenter/WC_ApplicationPackage/Email/procedures_NewFramework/AYSmoke/Webcenter/WC_ApplicationPackage/Email/WC_ApplicationAddFilter_Input.java
@@ -0,0 +1,85 @@
+package procedures_NewFramework.AYSmoke.Webcenter.WC_ApplicationPackage.Email;
+
+
+import or.OR;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import processor.CommonSeleniumActions;
+
+public class WC_ApplicationAddFilter_Input extends CommonSeleniumActions implements OR {
+	
+	@Test(description="This Procedure is used to perform some input operation in 'Application-Add Filter' page")
+	
+	public void WC_ApplicationAddFilter_Input(){
+		
+		try {
+			writeDetails();
+			Reporter.log("Proceed to retrieve Input Test Data for 'Application-Add Filter'Page");
+			String[] testdata = datacontainer;
+			String filtername = testdata[0];
+			String description = testdata[1];
+			String field = testdata[2];
+			String operator = testdata[3];
+			String value = testdata[4];
+			String adddeleterow = testdata[5];
+			String verifyvalue = testdata[6];
+			String savecancel = testdata[7];
+			//recentPopupSelect_without_window_nameWebdriver();
+			if(!filtername.equals("")){
+				Reporter.log("Enter the Filter Nmae as("+filtername+")");
+				sendKeys(attributeName_xpath, ET_FilterName, filtername);
+				
+			}
+			if(!description.equals("")){
+				Reporter.log("Enter the Description as("+description+")");
+				sendKeys(attributeName_xpath, ET_Description, description);
+				
+			}
+			if (!field.equals("")) {
+				Reporter.log("Select ("+field+")");
+				selectByVisibleTextWithSpaceWebdriver(attributeName_xpath, ET_FieldName, field);
+				//selectByVisibleTextWebdriver(attributeName_xpath, ET_FieldName, field);
+			}
+			if (!operator.equals("")) {
+				Reporter.log("Select ("+operator+")");
+				selectByVisibleTextWebdriver(attributeName_xpath, ET_Operator, operator);
+			}
+			
+			if (!value.equals("")) {
+				Reporter.log("Select ("+value+")");
+				waitForElementPresentWebdriver(attributeName_xpath, ET_Value, value);
+				clickWebdriver(attributeName_xpath, ET_Value);
+				waitForElementPresentWebdriver(attributeName_xpath, MCL_CheckValue+value+"']", value);
+				checkWebdriver(attributeName_xpath, MCL_CheckValue+value+"']");
+				clickWebdriver(attributeName_xpath, MQ_MulitSelectClose);
+			}
+			if(!verifyvalue.equals("")){
+				Reporter.log("Verify"+verifyvalue);
+				waitForElementPresentWebdriver(attributeName_xpath, ET_Value, verifyvalue);
+				clickWebdriver(attributeName_xpath, ET_Value);
+				verifyElementContainsTextWebdriver(attributeName_xpath, MCL_VerifyValue+verifyvalue+"')]", verifyvalue, verifyvalue);
+			}	
+			if (!savecancel.equals("")) {
+				Reporter.log("Clicking on : ("+savecancel+")");
+				if(savecancel.equalsIgnoreCase("add")){
+					waitForElementPresentWebdriver(attributeName_xpath,BR_Add, savecancel);
+					clickWebdriver(attributeName_xpath,BR_Add);
+				}
+				else if(savecancel.equalsIgnoreCase("save")){
+					waitForElementPresentWebdriver(attributeName_xpath,BR_Save,savecancel);
+					clickWebdriver(attributeName_xpath, BR_Save);
+				}
+				else if(savecancel.equalsIgnoreCase("cancel")){
+					clickWebdriver(attributeName_xpath, BR_Cancel);
+				}
+								
+			}
+				
+			} catch (Exception e) {
+				writeFailure(e.getLocalizedMessage());
+			}
+	}	
+}
+
---
+++ test/procedures_NewFramework/AYSmoke/Webcenter/WC_ApplicationPackage/Email/procedures_NewFramework/AYSmoke/Webcenter/WC_ApplicationPackage/Email/WC_ManageTemplates_Input.java
@@ -0,0 +1,68 @@
+package procedures_NewFramework.AYSmoke.Webcenter.WC_ApplicationPackage.Email;
+
+import or.OR;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import processor.CommonSeleniumActions;
+
+public class WC_ManageTemplates_Input extends CommonSeleniumActions implements OR {
+	
+
+	@Test(description="This Procedure is to perform some input operation in 'Manage Template' page")
+		public void WC_ManageTemplates_Input() throws Exception{
+		try {
+			writeDetails();
+//			headerStatement("PersonalData_Verify");
+//			startExecutionOrder(j);
+//			writeParametersTable();
+			Reporter.log("Proceed to retrieve Verify Test Data 'Manage Template' page");
+			String[]  fpvdatarepo = datacontainer;
+			String add_delete_folder = fpvdatarepo[0];
+			String link_click = fpvdatarepo[1];
+			String template_name = fpvdatarepo[2];
+			
+			Reporter.log("Verify Test Data was retrieved for ''Manage Template' page");
+			
+			if (!link_click.equals("")) {
+				//Reporter.log("Step 2 - Verify its navigate to ("+pagename+") page");
+				waitForElementPresentWebdriver(attributeName_xpath, Applpackage+link_click+"')]", link_click);
+				clickWebdriver(attributeName_xpath, Applpackage+link_click+"')]");
+				waitForPageToLoadWebdriver();
+			}
+			if (!add_delete_folder.equals("")) {
+				//Reporter.log("Step 1 - Verify Message("+message+") was displayed correctly");
+				if(add_delete_folder.equals("add")) {
+				waitForElementPresentWebdriver(attributeName_xpath, WC_AddDelMoveToFolder+add_delete_folder+"')]", add_delete_folder);
+				clickWebdriver(attributeName_xpath, WC_AddDelMoveToFolder+add_delete_folder+"')]");
+				}
+				else
+				{
+					//waitForElementPresentWebdriver(attributeName_xpath, AMET_CheckboxTemplate, "TestMail");
+					clickWebdriver(attributeName_xpath, AMET_CheckboxTemplate+template_name+"']]/preceding-sibling::td/input");
+					clickWebdriver(attributeName_xpath, WC_AddDelMoveToFolder+add_delete_folder+"')]");
+					try {
+				        Alert alert = driver.switchTo().alert();
+				        String AlertText = alert.getText();
+				        System.out.println(AlertText);
+				        alert.accept();
+				    } catch (Exception e) {
+				        System.out.println("no alert");
+				    }
+				}
+			}
+			
+			
+			
+		} catch (Exception e) {
+			writeFailure(e.getLocalizedMessage());
+		}
+		//endExecutionOrder(j);
+//		Reporter.log("##########End of Execution Order ["+j+"]##########");
+	}
+
+}
